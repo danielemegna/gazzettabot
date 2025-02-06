c224144cc1cd207cd8066ee4ff84a421435eba46
Daniele Megna <daniele.megna@qmates.tech>
2025-02-06 07:53:25 +0100

    WIP on CliXdccBridge integration tests

--- src/cli_xdcc_bridge.go
+++ src/cli_xdcc_bridge.go
@@ -1,7 +1,26 @@
 package gazzettabot
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 type CliXdccBridge struct{}
 
 func (this CliXdccBridge) Search(query string) []IrcFile {
+	log.Printf("Performing search for query [%s] ...", query)
+
+	wd, _ := os.Getwd()
+	log.Println(wd)
+
+	var cmd = exec.Command("??./lib/xdcc", query)
+	var out, err = cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var outputTable = string(out)
+	log.Println(outputTable)
 	return []IrcFile{}
 }
--- test/integration/cli_xdcc_bridge_test.go
+++ test/integration/cli_xdcc_bridge_test.go
@@ -1,10 +1,14 @@
 package gazzettabot_test
 
 import (
+	. "danielemegna/gazzettabot/src"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestTestingIsWorking(t *testing.T) {
-	assert.True(t, true)
+func TestSearchWithoutResults(t *testing.T) {
+	var bridge = CliXdccBridge{}
+	var files = bridge.Search("impossibile search string 19091990")
+	assert.Empty(t, files)
+	assert.Equal(t, []IrcFile{}, files)
 }
