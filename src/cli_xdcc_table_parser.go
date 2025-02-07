06707c3b908cd75c4ba6074868f2a9e17fcbc7d7
Daniele Megna <megna.dany@gmail.com>
2025-02-07 18:08:44 +0100

    Parsing table output results

--- src/cli_xdcc_table_parser.go
+++ src/cli_xdcc_table_parser.go
@@ -1,5 +1,24 @@
 package gazzettabot
 
+import (
+	"github.com/samber/lo"
+	"strings"
+)
+
 func ParseTable(tableString string) []IrcFile {
-	return []IrcFile{}
+	var rows = strings.Split(tableString, "\n")
+	if len(rows) < 5 {
+		return []IrcFile{}
+	}
+
+	var rowsWithoutHeaders = rows[3:]
+	var foundFiles = rowsWithoutHeaders[:len(rowsWithoutHeaders)-2]
+	return lo.Map(foundFiles, func(row string, _ int) IrcFile {
+		var parts = strings.Split(row, "|")
+		return IrcFile{
+			Name: strings.TrimSpace(parts[1]),
+			SizeInMegaByte: 74,
+			Url: "irc://irc.arabaphenix.it/#arabafenice/ArA|Edicola|01/146",
+		}
+	})
 }
--- test/unit/cli_xdcc_table_parser_test.go
+++ test/unit/cli_xdcc_table_parser_test.go
@@ -34,6 +34,15 @@ func TestParseEmptyTable(t *testing.T) {
 	assert.Equal(t, []IrcFile{}, files)
 }
 
+func TestParseFullTable(t *testing.T) {
+	var files = ParseTable(asSingleString(FULL_TABLE_STRING))
+	assert.Len(t, files, 7)
+	assert.Equal(t, "La.Gazzetta.dello.Sport.Ed.Bologna.COMPLETA.3.Febbraio.2025.pdf", files[0].Name)
+	assert.Equal(t, 74, files[0].SizeInMegaByte)
+	assert.Equal(t, "irc://irc.arabaphenix.it/#arabafenice/ArA|Edicola|01/146", files[0].Url)
+	assert.Equal(t, "La.Gazzetta.dello.Sport.Ed.Cagliari.COMPLETA.3.Febbraio.2025.pdf", files[1].Name)
+}
+
 func asSingleString(inputLines []string) string {
 	return strings.Join(inputLines, "\n") + "\n"
 }
