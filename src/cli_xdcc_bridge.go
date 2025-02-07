8462ff2db914d0f1aaa896164223a1eebe590eb2
Daniele Megna <megna.dany@gmail.com>
2025-02-07 15:03:50 +0100

    Use XDCC_BINARY env variable to handle xdcc binary filepath location

--- src/cli_xdcc_bridge.go
+++ src/cli_xdcc_bridge.go
@@ -11,10 +11,12 @@ type CliXdccBridge struct{}
 func (this CliXdccBridge) Search(query string) []IrcFile {
 	log.Printf("Performing search for query [%s] ...", query)
 
-	wd, _ := os.Getwd()
-	log.Println(wd)
+	var xdccBinary, xdccBinaryDefined = os.LookupEnv("XDCC_BINARY")
+	if(!xdccBinaryDefined) {
+		log.Fatal("XDCC_BINARY environment variable not defined!")
+	}
 
-	var cmd = exec.Command("??./lib/xdcc", query)
+	var cmd = exec.Command(xdccBinary, "search", query)
 	var out, err = cmd.Output()
 	if err != nil {
 		log.Fatal(err)
