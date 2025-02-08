25b9e7e9704fe9d2ad662df27df275b421f73580
Daniele Megna <daniele.megna@qmates.tech>
2025-02-08 12:52:37 +0100

    Refactoring in CliXdccBridge#Search

--- src/cli_xdcc_bridge.go
+++ src/cli_xdcc_bridge.go
@@ -9,19 +9,27 @@ import (
 type CliXdccBridge struct{}
 
 func (this CliXdccBridge) Search(query string) []IrcFile {
-	log.Printf("Performing search for query [%s] ...", query)
+	log.Printf("Performing search for query [%s] ...\n", query)
+	var xdccBinaryFilepath = xdccBinaryFilepathFromEnv()
+	var outputString = execSearch(xdccBinaryFilepath, query)
+	var files = ParseTable(outputString)
+	log.Printf("Found %d files!\n", len(files))
+	return files
+}
 
-	var xdccBinary, xdccBinaryDefined = os.LookupEnv("XDCC_BINARY")
-	if !xdccBinaryDefined {
-		log.Fatal("XDCC_BINARY environment variable not defined!")
-	}
-
-	var cmd = exec.Command(xdccBinary, "search", query)
-	var out, err = cmd.Output()
+func execSearch(xdccBinaryFilepath string, query string) string {
+	var command = exec.Command(xdccBinaryFilepath, "search", query)
+	var commandOutput, err = command.Output()
 	if err != nil {
 		log.Fatal(err)
 	}
+	return string(commandOutput)
+}
 
-	var outputTable = string(out)
-	return ParseTable(outputTable)
+func xdccBinaryFilepathFromEnv() string {
+	var xdccBinaryFilepath, xdccBinaryDefined = os.LookupEnv("XDCC_BINARY")
+	if !xdccBinaryDefined {
+		log.Fatal("XDCC_BINARY environment variable not defined!")
+	}
+	return xdccBinaryFilepath
 }
