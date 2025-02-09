2ea014d23a00cb2773fef33f70d6fd857487dffd
Daniele Megna <daniele.megna@qmates.tech>
2025-02-09 18:21:00 +0100

    Add update script for production pull & rebuild

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )";)
+
+git pull --rebase --autostash
+docker run --rm -v $SCRIPT_DIR:/app -w /app golang:1.23.3-alpine go build -o bin/main main.go
