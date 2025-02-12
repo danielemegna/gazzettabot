584d3380daf1c845120863ecdfa5cb22274f9826
Daniele Megna <megna.dany@gmail.com>
2025-02-12 09:07:55 +0100

    Enable run of update script from every location

--- update.sh
+++ update.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 set -x
+
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )";)
+cd $SCRIPT_DIR
 
 git pull --rebase --autostash
 
 # ======== to run a slower unoptimized build from golang official image
-#docker run --rm -v $SCRIPT_DIR:/app -w /app golang:1.23.3-alpine go build -o bin/main main.go
+#docker run --rm -v $PWD:/app -w /app golang:1.23.3-alpine go build -o bin/main main.go
 
 # ======== to create a gazzettabuild custom image to optimize following builds
-#docker run --name gazzettabuild-container -v $SCRIPT_DIR:/app -w /app golang:1.23.3-alpine go build -o bin/main main.go
+#docker run --name gazzettabuild-container -v $PWD:/app -w /app golang:1.23.3-alpine go build -o bin/main main.go
 #docker commit gazzettabuild-container gazzettabuild && docker rm gazzettabuild-container && docker rmi golang:1.23.3-alpine
 
 # ======== to build with the optimized gazzzettabuild custom image
-docker run --rm -v $SCRIPT_DIR:/app -w /app gazzettabuild go build -o bin/main main.go
+docker run --rm -v $PWD:/app -w /app gazzettabuild go build -o bin/main main.go
+
