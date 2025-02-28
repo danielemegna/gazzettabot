91ce5be53e7ebe08f577c6b0ccd43d166c03a387
Daniele Megna <megna.dany@gmail.com>
2025-02-28 08:21:30 +0100

    Completed search_query#GazzettaDelloSportSearchQueryFor(Time)

--- src/search_query.go
+++ src/search_query.go
@@ -7,8 +7,18 @@ import (
 )
 
 func GazzettaDelloSportSearchQueryFor(date time.Time) string {
-	var todayDay = date.Day()
-	return "Gazzetta dello Sport " + strconv.Itoa(todayDay) + " Febbraio" + " -" + generateTimestampID()
+	var todayDayString = strconv.Itoa(date.Day())
+	var todayMonthString = italianNameFor(date.Month())
+	var dateString = todayDayString + " " + todayMonthString
+	return "Gazzetta dello Sport " + dateString + " -" + generateTimestampID()
+}
+
+func italianNameFor(month time.Month) string {
+	var lookup = []string{
+		"Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
+		"Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre",
+	}
+	return lookup[month-1]
 }
 
 func generateTimestampID() string {
--- test/unit/search_query_test.go
+++ test/unit/search_query_test.go
@@ -3,12 +3,35 @@ package gazzettabot_test
 import (
 	. "danielemegna/gazzettabot/src"
 	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
 	"time"
 )
 
+func TestGenerateWithKnownDate(t *testing.T) {
+	var testCases = []struct {
+		month time.Month
+		day   int
+		regex string
+	}{
+		{time.January, 22, `^Gazzetta dello Sport 22 Gennaio -\d{6}$`},
+		{time.March, 14, `^Gazzetta dello Sport 14 Marzo -\d{6}$`},
+		{time.June, 3, `^Gazzetta dello Sport 3 Giugno -\d{6}$`},
+		{time.September, 19, `^Gazzetta dello Sport 19 Settembre -\d{6}$`},
+		{time.December, 25, `^Gazzetta dello Sport 25 Dicembre -\d{6}$`},
+	}
+
+	for index, testCase := range testCases {
+		t.Run("Test case #"+strconv.Itoa(index+1), func(t *testing.T) {
+			var date = time.Date(2024, testCase.month, testCase.day, 6, 0, 0, 0, time.Local)
+			var searchQuery = GazzettaDelloSportSearchQueryFor(date)
+			assert.Regexp(t, testCase.regex, searchQuery)
+		})
+	}
+}
+
 func TestGenerateWithNowDate(t *testing.T) {
 	var searchQuery = GazzettaDelloSportSearchQueryFor(time.Now())
-	assert.Greater(t, len(searchQuery), 15)
+	assert.Greater(t, len(searchQuery), 30)
 	assert.Regexp(t, `^Gazzetta dello Sport \d{2} \S+ -\d{6}$`, searchQuery)
 }
